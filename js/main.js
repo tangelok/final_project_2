92143b46ea313e2904efc0d8ac6d6f15ac345b7e
Katie Angelo <krineangelo@gmail.com>
2017-08-08 20:39:30 -0400

    New Home Page

--- js/main.js
+++ js/main.js
@@ -3,8 +3,9 @@
 /* ----------------------------------------
 Main
 ------------------------------------------- */
- // If you click on the .reservation button
+ // If you click on the reservation button
   //Then a pop-up will appear allowing you to make a reservation through OpenTable
+//
 
   $('.about button').on('click', function (){
     $('.sidebar').toggleClass('active');
@@ -12,7 +13,15 @@ Main
   $('.close').on('click', function(){
     $('.sidebar').removeClass('active');
   });
-  
+
+
+  //If you have scrolled x pixels
+    //Then the nav bar will be fixed
+  //else the nav bar will scroll with screen
+
+  //If you have scrolled x pixels
+    // Then image ____ will fade In
+  //Else
 /* ----------------------------------------
 About Ming
 ------------------------------------------- */
