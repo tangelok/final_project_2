7d426ccf1a0e567988d02ff063b32fd4d4521334
Katie Angelo <krineangelo@gmail.com>
2017-08-11 17:34:39 -0400

    About Ming updated

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@
 /* ----------------------------------------
 Main
 ------------------------------------------- */
- // If you click on the reservation button
+ // If you click on the reservation button (.header-reservations) (.nav-reservations)
   //Then a pop-up will appear allowing you to make a reservation through OpenTable
 //
 
@@ -14,14 +14,18 @@ Main
     $('.sidebar').removeClass('active');
   });
 
-
-  //If you have scrolled x pixels
-    //Then the nav bar will be fixed
-  //else the nav bar will scroll with screen
-
   //If you have scrolled x pixels
     // Then image ____ will fade In
   //Else
+
+  //If you click on the (.view-menu) button
+    //Then it will take you to menu.html
+
 /* ----------------------------------------
-About Ming
+About Blue Dragon
 ------------------------------------------- */
+//If you click on the PayPal option
+  //Then you will be able to pay through PayPal
+
+//If you click on the (.view-catering-menu) button
+  //Then it will take you to the menu.html
