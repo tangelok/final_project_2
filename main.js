17893a99faf7ee3194ccda8612a90bf3ef04a88f
Katie Angelo <krineangelo@gmail.com>
2017-08-12 14:13:38 -0400

    Pseudo Code

---
+++ main.js
@@ -0,0 +1,54 @@
+// Make it rain!
+
+/* ----------------------------------------
+Main
+------------------------------------------- */
+
+  $('.about button').on('click', function (){
+    $('.sidebar').toggleClass('active');
+  });
+  $('.close').on('click', function(){
+    $('.sidebar').removeClass('active');
+  });
+  
+  // If you click on the reservation button (.header-reservations) (.nav-reservations)
+   //Then a pop-up will appear allowing you to make a reservation through OpenTable
+
+  //If you have scrolled x pixels
+    // Then image ____ will fade In
+  //Else
+
+  //If you click on the (.view-menu) button
+    //Then it will take you to menu.html
+
+//Make a image carousel for the header
+
+//If you click on one of the nav links
+  //Then that will be a different color in the nav Bar
+
+//If you click on the footer "location"
+  //Then it will bring you to directions in contact
+
+/* ----------------------------------------
+About Blue Dragon
+------------------------------------------- */
+//If you click on the PayPal option
+  //Then you will be able to pay through PayPal
+
+//If you click on the (.view-catering-menu) button
+  //Then it will take you to the menu.html
+
+//If you have scrolled x pixels
+  // Then image ____ will fade In
+//Else
+
+
+  /* ----------------------------------------
+  About Ming
+  ------------------------------------------- */
+  // If you hover over any of the recipes
+    //Then it will flip over to the recipe and directions on how to make it
+
+    //If you have scrolled x pixels
+      // Then image ____ will fade In
+    //Else
